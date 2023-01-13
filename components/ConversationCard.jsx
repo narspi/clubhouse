f526179f467a07fa5cf691c0c2e63beb11f1518c
narspi <ivanovmichurina@gmail.com>
2023-01-13 08:16:34 +0300

    add conversation card

--- components/ConversationCard.jsx
+++ components/ConversationCard.jsx
@@ -1,9 +1,57 @@
 import Image from "next/image";
+import { ChatBubbleOvalLeftEllipsisIcon } from "@heroicons/react/20/solid";
+import { UserIcon } from "@heroicons/react/20/solid";
+import Link from 'next/link';
 
-export const ConversationCard = () => {
-    return (
-        <div className="bg-white rounded-3xl p-4">
-            bla
+export const ConversationCard = ({
+  title,
+  avatars,
+  guests,
+  guestsCount,
+  speakersCount,
+}) => {
+  return (
+    <Link className="bg-white rounded-3xl p-6" href='/profile/12'>
+      <h3 className="font-bold text-xl mb-2">{title}</h3>
+      <div className="flex items-start">
+        <div className="relative w-10 h-10">
+          <div className="absolute w-10 h-10">
+            <Image
+              src={avatars[0]}
+              fill
+              alt="one"
+              className="object-cover object-center rounded-full"
+            />
+          </div>
+          <div className="absolute w-10 h-10 top-6 left-4">
+            <Image
+              src={avatars[1]}
+              fill
+              alt="one"
+              className="object-cover object-center rounded-full"
+            />
+          </div>
         </div>
-    )
-}
+        <div className="ml-8">
+          {guests && (
+            <ul>
+              {guests.map((item) => (
+                <li key={item}>{item}</li>
+              ))}
+            </ul>
+          )}
+          <div className="opacity-50">
+            <span className="inline-block mr-2">
+              <span className="inline-block align-middle">{speakersCount}</span>
+              <UserIcon className="w-4 h-4 inline-block align-middle" />
+            </span>
+            <span className="inline-block">
+              <span className="inline-block align-middle">{guestsCount}</span>
+              <ChatBubbleOvalLeftEllipsisIcon className="w-4 h-4 inline-block align-middle" />
+            </span>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+};
--- pages/rooms.jsx
+++ pages/rooms.jsx
@@ -19,10 +19,90 @@ const Rooms = () => {
             </button>
           </div>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 my-4 gap-4">
-            <ConversationCard />
-            <ConversationCard />
-            <ConversationCard />
-            <ConversationCard />
+            <ConversationCard
+              title="Создаю клон клабхауз"
+              avatars={[
+                "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+                "https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"
+              ]}
+              guests={[
+                'Клаудиа шифер',
+                'Старый бог',
+                'Матумба мен'
+              ]}
+              guestsCount={12}
+              speakersCount={3}
+            />
+            <ConversationCard
+              title="Создаю клон клабхауз"
+              avatars={[
+                "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+                "https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"
+              ]}
+              guests={[
+                'Клаудиа шифер',
+                'Старый бог',
+                'Матумба мен'
+              ]}
+              guestsCount={12}
+              speakersCount={3}
+            />
+            <ConversationCard
+              title="Создаю клон клабхауз"
+              avatars={[
+                "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+                "https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"
+              ]}
+              guests={[
+                'Клаудиа шифер',
+                'Старый бог',
+                'Матумба мен'
+              ]}
+              guestsCount={12}
+              speakersCount={3}
+            />
+            <ConversationCard
+              title="Создаю клон клабхауз"
+              avatars={[
+                "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+                "https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"
+              ]}
+              guests={[
+                'Клаудиа шифер',
+                'Старый бог',
+                'Матумба мен'
+              ]}
+              guestsCount={12}
+              speakersCount={3}
+            />
+            <ConversationCard
+              title="Создаю клон клабхауз"
+              avatars={[
+                "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+                "https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"
+              ]}
+              guests={[
+                'Клаудиа шифер',
+                'Старый бог',
+                'Матумба мен'
+              ]}
+              guestsCount={12}
+              speakersCount={3}
+            />
+            <ConversationCard
+              title="Создаю клон клабхауз"
+              avatars={[
+                "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+                "https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"
+              ]}
+              guests={[
+                'Клаудиа шифер',
+                'Старый бог',
+                'Матумба мен'
+              ]}
+              guestsCount={12}
+              speakersCount={3}
+            />
           </div>
         </div>
       </div>
