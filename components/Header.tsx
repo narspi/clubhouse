8e59931d9607cf9eb2d26fe1b1a560d597afd760
narspi <ivanovmichurina@gmail.com>
2024-04-17 18:24:33 +0300

    fix next.js ts confgi

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,19 @@
 import Image from "next/image";
-import Link from 'next/link';
+import Link from "next/link";
 
-export const Header = ()=> {
-    return (
-        <div className="p-4 border-b-2">
-            <div className="container mx-auto flex justify-between items-center">
-                <Link href="/rooms">
-                    <Image src="/logo.png" width={200} height={32} alt="logo"/>
-                    <span></span>
-                </Link>
-                <div className="flex items-center">
-                    <span className="mr-2 font-bold">Archackov denis</span>
-                    <Image src="/anknown.svg" width={50} height={50} alt="avatar"/>
-                </div>
-            </div>
+export const Header = () => {
+  return (
+    <div className="p-4 border-b-2">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link href="/rooms">
+          <Image src="/logo.png" width={200} height={32} alt="logo" />
+          <span></span>
+        </Link>
+        <div className="flex items-center">
+          <span className="mr-2 font-bold">Archackov denis</span>
+          <Image src="/anknown.svg" width={50} height={50} alt="avatar" />
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
